3fc19fcccfd5a3b6d2dade621478a1f78ebd8607
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-11 16:16:35 +0100

    feat: modified public/index.html

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -8,35 +8,27 @@ class Login extends HTMLElement {
     connectedCallback() {
         // Move the HTML content into the shadow DOM
         this.innerHTML = ` 
-            <div id="show">
+              
                 <div class="login-box">
-                    <a href="#" id="hide">X</a>
+                    <a href="#" id="hide" onclick="hideLogin()" >X</a>
                     <img class="logo" src="./icons/pin.png" alt="Pinterest Logo">
                     <input class="form-input" type="email" placeholder="Email">
                     <input class="form-input" type="password" placeholder="Password">
-                    <button class="login-btn" id="login" onclick=showLogin() >Login</button>
+                    <button class="login-btn" id="login" >Login</button>
                     <div class="social-login">
                         <a href="#" class="social-btn">Login with Facebook</a>
                         <a href="#" class="social-btn">Login with GitHub</a>
                         <a href="#" class="social-btn">Login with Google</a>
                     </div>
                 </div>
-            </div>
+               
+          
         `;
 
         // Now, you can access the elements within the shadow DOM
     }
 
   
-}
-
-function  showLogin(){
-    var x = document.getElementById('show')
-        if(x.style.display === "none"){
-            x.style.display = "block"
-        }else{
-            x.style.display = "none"
-        }
 }
 
 
