81f922d6d98dae160913712b980bbc341f396e2a
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-11 13:25:52 +0100

    feat: added the gitignore files

--- build/app.js
+++ build/app.js
@@ -3,8 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.send('Hello Pinterest');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.listen(port, () => {
     console.log('Server started on port ' + port);
