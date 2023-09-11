161f46cc600e07bdd303de152c970fb02fb50a7e
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-11 15:43:38 +0100

    feat: added more features

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,24 +1,44 @@
-class Login extends HTMLElement{
-    constructor(){
-        super()
+class Login extends HTMLElement {
+    constructor() {
+        super();
+        // Create a shadow DOM for this custom element
+       
     }
 
-    connectedCallback(){
-        this.innerHTML = `
-        <div class="login-box">
-        <img class="logo" src="./icons/pin.png" alt="Pinterest Logo">
-        <input class="form-input" type="email" placeholder="Email">
-        <input class="form-input" type="password" placeholder="Password">
-        <button class="login-btn">Login</button>
-        <div class="social-login">
-            <a href="#" class="social-btn">Login with Facebook</a>
-            <a href="#" class="social-btn">Login with GitHub</a>
-            <a href="#" class="social-btn">Login with Google</a>
-        </div>
-    </div>
-        `
+    connectedCallback() {
+        // Move the HTML content into the shadow DOM
+        this.innerHTML = ` 
+            <div id="show">
+                <div class="login-box">
+                    <a href="#" id="hide">X</a>
+                    <img class="logo" src="./icons/pin.png" alt="Pinterest Logo">
+                    <input class="form-input" type="email" placeholder="Email">
+                    <input class="form-input" type="password" placeholder="Password">
+                    <button class="login-btn" id="login" onclick=showLogin() >Login</button>
+                    <div class="social-login">
+                        <a href="#" class="social-btn">Login with Facebook</a>
+                        <a href="#" class="social-btn">Login with GitHub</a>
+                        <a href="#" class="social-btn">Login with Google</a>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        // Now, you can access the elements within the shadow DOM
     }
+
+  
 }
-console.log('This is showing please!')
+
+function  showLogin(){
+    var x = document.getElementById('show')
+        if(x.style.display === "none"){
+            x.style.display = "block"
+        }else{
+            x.style.display = "none"
+        }
+}
+
+
 
 customElements.define('login-element', Login);
