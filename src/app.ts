db33d0f78e9212dc3e329fa83467111de2e5db96
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-12 16:38:06 +0100

    feat: modified files

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
-import { Request, Response, urlencoded } from "express";
+import { NextFunction, Request, Response, urlencoded } from "express";
 const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const db = require('./db/db')
+const db = require('./db/db');
+const passport = require('passport')
+const LocalStrtegy = require('passport-local').Strategy;
+
+passport.use(
+    new LocalStrtegy(async (username: any, password: any, done: any) => {
+        
+        const user = await db.query('SELECT username, password FROM users WHERE username=$1 AND password=$2', 
+        [username, password]);
+
+        const name = user.rows[0].username;
+        const pin = user.rows[0].password;
+        console.log(name, pin);
+    })
+)
+
 
 app.use(express.static('public'))
 app.use(urlencoded({ extended: true}));
@@ -13,11 +28,24 @@ app.get('/', (req:Request, res:Response) => {
 });
 
 
-app.post('/login', async (req:Request, res:Response) => {
-    const result = await db.query('SELECT * FROM Users')
-    console.log(result.rows)
+app.post(
+    '/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login'
+    })
+)
 
-});
+const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    if(req.isAuthenticated()){
+        return next();
+    }
+}
+
+
+app.get('/profile', (req:Request, res:Response) => {
+    res.send('Welcome to Profile!')
+})
 
 app.listen(port, () => {
     console.log('Server started on port ' + port)
