0c9b296d5c6278acad64291d9f3cfabd08184a1a
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-13 12:44:22 +0100

    fix/feat: modified files, fix the failed to serialize user error add many feautes

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,54 @@
+import { error } from "console";
 import { NextFunction, Request, Response, urlencoded } from "express";
 const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
 const db = require('./db/db');
-const passport = require('passport')
-const LocalStrtegy = require('passport-local').Strategy;
+const passport = require('passport');
+import { Strategy as LocalStrategy } from 'passport-local'
+const session = require('express-session');
+
+app.use(
+    session({
+      secret: 'abc',
+      resave: false,
+      saveUninitialized: false,
+      // Additional session options as needed
+    })
+  );
+
+app.use(passport.session());
+app.use(passport.initialize());
+
 
 passport.use(
-    new LocalStrtegy(async (username: any, password: any, done: any) => {
-        
-        const user = await db.query('SELECT username, password FROM users WHERE username=$1 AND password=$2', 
-        [username, password]);
+    new LocalStrategy(
+      { usernameField: 'email' },
+      (email, password, done) => {
+        // Query your PostgreSQL database to find the user
+        db.query('SELECT userid FROM Users WHERE email = $1 AND password = $2', [email, password])
+          .then((result: any) => {
+            const user = result.rows[0];
+            if (!user) {
+              return done(null, false, { message: 'Incorrect username or password.' });
+            }
+            return done(null, user); // Serialize the user by storing their ID
+          })
+          .catch((err: Error) => done(err));
+      }
+    )
+  );
 
-        const name = user.rows[0].username;
-        const pin = user.rows[0].password;
-        console.log(name, pin);
-    })
-)
+  
+  passport.serializeUser(function(user:any, done:any) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user:any, done:any) {
+    done(null, user);
+  });
+  
 
 
 app.use(express.static('public'))
@@ -28,24 +59,25 @@ app.get('/', (req:Request, res:Response) => {
 });
 
 
-app.post(
-    '/login',
-    passport.authenticate('local', {
-        successRedirect: '/',
-        failureRedirect: '/login'
-    })
-)
+app.post("/login", passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/fail'
+}));
+  
+  
 
-const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+app.get('/fail', (req:Request, res:Response) => {
+    
     if(req.isAuthenticated()){
-        return next();
+        return res.send('The request is ok')
+    }else{
+        res.send('There is a failure please!');
+        return
     }
-}
+    
+});
 
 
-app.get('/profile', (req:Request, res:Response) => {
-    res.send('Welcome to Profile!')
-})
 
 app.listen(port, () => {
     console.log('Server started on port ' + port)
